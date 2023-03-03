6fa28a5289ae2fca8ec0be1bb103e2ce7ff8f7ba
vn548ge <fahd.tahir@walmart.com>
2023-03-03 16:28:26 +0500

    kickstart

---
+++ dist/app.js
@@ -0,0 +1,12 @@
+"use strict";
+function add(number1, number2) {
+    console.log(number1 + number2);
+}
+const number1 = 5;
+const number2 = 6;
+const result = add(number1, number2);
+let age = 20;
+if (age >= 50) {
+    age += 10;
+}
+//# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,18 @@
+function add(number1: number, number2: number){
+    console.log(number1 + number2)
+}
+
+
+const number1 = 5;
+const number2 = 6;
+const result = add(number1, number2);
+
+//Object Types
+
+let age = 20;
+if(age >= 50){
+    age += 10;
+}
+
+
+
