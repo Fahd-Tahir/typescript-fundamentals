cbafcc3ba8fd0a2a9ebe6d2959c61ba04013b48f
vn548ge <fahd.tahir@walmart.com>
2023-03-03 17:04:31 +0500

    tuple init and extraction

---
+++ src/app.js
@@ -0,0 +1,28 @@
+// fundamentals
+// Node                                TypeScript
+// number                              any
+// string                              unknown
+// boolean                             never
+// null                                enum
+// undefined                           tuple
+// object                
+var sales = 123234325342;
+var course = "TypeScript";
+var is_published = false;
+// we can add underscore in number to make it more readable
+var level;
+// if we do not specify a value to type to a var TS set it to any
+function render(document) {
+    console.log(document);
+}
+// Arrays
+// let numbers: number[] = [1,2,3,4,5];
+var numbers = [];
+numbers[0] = 1;
+numbers[1] = 2;
+// fetching
+numbers.forEach(function (n) { return n.toString; });
+// TS new type is tuple, fixed length array where each element has particular type, oftenyl use when working with values.
+// Tuples
+var couple = [1, "Fahd"];
+console.log(couple[1]);
--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,41 @@
-function add(number1: number, number2: number){
-    console.log(number1 + number2)
+// fundamentals
+
+// Node                                TypeScript
+// number                              any
+// string                              unknown
+// boolean                             never
+// null                                enum
+// undefined                           tuple
+// object                
+
+let sales: number = 123_234_325_342;
+let course: string = "TypeScript";
+let is_published: boolean = false;
+
+// we can add underscore in number to make it more readable
+
+let level;
+
+// if we do not specify a value to type to a var TS set it to any
+
+function render(document){
+    console.log(document);
 }
 
+// Arrays
+// let numbers: number[] = [1,2,3,4,5];
+let numbers = [];
+numbers[0] = 1;
+numbers[1] = 2;
 
-const number1 = 5;
-const number2 = 6;
-const result = add(number1, number2);
+// fetching
 
-//Object Types
+numbers.forEach(n => n.toString);
 
-let age = 20;
-if(age >= 50){
-    age += 10;
-}
+// TS new type is tuple, fixed length array where each element has particular type, oftenyl use when working with values.
 
+// Tuples
 
+let couple : [number, string] = [1, "Fahd"];
+console.log(couple[1]);
 
