968be8fec72917009d418cd2b73d0286773fb606
vn548ge <fahd.tahir@walmart.com>
2023-03-07 19:53:41 +0500

    operational operator

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,5 @@
 "use strict";
+var _a;
 let sales = 123234325342;
 let course = "TypeScript";
 let is_published = false;
@@ -26,5 +27,39 @@ function calculateIncome(income, taxYear = 2022) {
     }
 }
 calculateIncome(10000);
-let employee = { id: 1, name: "Mosh" };
+let employee = {
+    id: 1,
+    name: "Mosh",
+    retire: (date) => {
+        console.log(date);
+    }
+};
+function kgToLbs(weight) {
+    if (typeof weight === 'number')
+        return weight * 2.2;
+    else
+        return parseInt(weight) * 2.2;
+}
+kgToLbs(100);
+kgToLbs("100kgs");
+let textBox = {
+    drag: () => { },
+    resize: () => { },
+};
+let weight = 50;
+let measure = 'inch';
+function greetings(name) {
+    if (name) {
+        console.log(name.concat(' Hola'));
+    }
+    else
+        console.log("Hola");
+}
+greetings(null);
+greetings("react");
+function getCustomer(id) {
+    return id == 0 ? null : { birthday: new Date() };
+}
+let customer = getCustomer(1);
+console.log((_a = customer === null || customer === void 0 ? void 0 : customer.birthday) === null || _a === void 0 ? void 0 : _a.getFullYear);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -65,9 +65,88 @@ function calculateIncome(income : number, taxYear = 2022) : number{
 
 calculateIncome(10_000); 
 
-// Objects
+//Object // type alias
 
-let employee: {
+type Employee = {
     readonly id: number,
-    name: string
-} = { id : 1, name: "Mosh"}
+    name: string,
+    retire: (date: Date) => void,
+}
+
+let employee : Employee = { 
+    id : 1,
+    name: "Mosh", 
+    retire: (date: Date) => {
+    console.log(date);
+}}
+
+// union operators
+// an input can be either on of the two objects.
+
+function kgToLbs(weight: number | string){
+    if(typeof weight === 'number')
+        return weight * 2.2;
+    else
+        return parseInt(weight) * 2.2;
+}
+
+kgToLbs(100);
+kgToLbs("100kgs");
+
+// intersection operators
+// let say we have two objects and a function is both so that function is called Intersection
+
+type Draggable = {
+    drag: () => void;
+}
+
+type Resizeable = {
+    resize: () => void;
+}
+
+type UiWidget = Resizeable | Draggable;
+
+let textBox : UiWidget = {
+    drag: () => {},
+    resize: () => {},
+}
+
+// Literals
+// let say we have a variable and its value can be one out of the range i.e. 50 or 100 but not other than that we use literals for that.
+
+// first example
+let weight: 50 = 50;
+
+// using typeAlias
+type Measurements = 'inch' | 'centimeter';
+
+//success
+let measure: Measurements = 'inch'
+//failure
+// let waist: Measurements = 'millimeter'
+
+// nullable
+
+function greetings(name: string | null | undefined){
+    if(name){
+        console.log(name.concat(' Hola'))
+    }else 
+        console.log("Hola")
+}
+
+greetings(null);
+
+greetings("react");
+
+// Optional operator for null
+
+type Customer = {
+    birthday ?: Date
+};
+
+function getCustomer(id: number): Customer| null | undefined {
+    return id == 0 ? null : {birthday : new Date()}
+}
+
+let customer = getCustomer(1);
+console.log(customer?.birthday?.getFullYear);
